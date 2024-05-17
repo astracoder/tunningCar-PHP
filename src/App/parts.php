891db68fb03cdb867b660a1b897d4afffe18d79d
Drazic <cy4kqq@hotmail.com>
2024-05-17 17:48:52 -0300

    Add sites

---
+++ src/App/parts.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Drazic Tunning Car</title>
+</head>
+<body>
+    <?php
+        include '../Components/menu.php';
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 m-3 p-3">
+            <h1 class="text-center mb-3">Drazic Tunning Car</h1>
+            <h4 class="text-light bg-dark">Lista de peças</h4>
+        </div>
+    </div>
+</body>
+</html>
